7255c9b7da6cf31a3961fed1e3b9b8aacae44343
nhittyn <hudinhdong@gmail.com>
2025-09-24 22:56:58 +0700

    schema: add user

---
+++ backend/prisma/migrations/20250924155552_add_user/migration.sql
@@ -0,0 +1,24 @@
+-- CreateEnum
+CREATE TYPE "public"."UserStatus" AS ENUM ('ACTIVE', 'UNVERIFIED', 'LOCKED');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT,
+    "status" "public"."UserStatus" NOT NULL DEFAULT 'UNVERIFIED',
+    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "public"."User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "public"."User"("phone");
