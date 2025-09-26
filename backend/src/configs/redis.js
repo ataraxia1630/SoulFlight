6fd19dde0f29d2f83d8754632e403809a31cea02
nhittyn <hudinhdong@gmail.com>
2025-09-26 14:33:35 +0700

    package: install & setup redis

---
+++ backend/src/configs/redis.js
@@ -0,0 +1,25 @@
+const { createClient } = require('redis');
+
+const redis = createClient({
+  username: 'default',
+  password: process.env.REDIS_PASS,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+});
+
+redis.on('error', (err) => console.log('Redis Client Error', err));
+
+async function connectRedis() {
+  try {
+    await redis.connect();
+    console.log('✅ Đã kết nối redis thành công!');
+  } catch (error) {
+    console.error('❌ Kết nối redis thất bại:', error.message);
+  }
+}
+
+connectRedis();
+
+module.exports = redis;
