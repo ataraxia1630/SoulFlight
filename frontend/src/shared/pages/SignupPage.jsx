d112a8213e8536bd02e359fb639830f29822c1d9
nhittyn <hudinhdong@gmail.com>
2025-09-30 22:10:11 +0700

    feat: handle sendOtp and verifyOtp

--- frontend/src/shared/pages/SignupPage.jsx
+++ frontend/src/shared/pages/SignupPage.jsx
@@ -1,17 +1,27 @@
-import { useState } from "react";
-import SignupForm from "@/shared/components/auth/SignupForm";
-import useSocialAuth from "@/shared/hooks/useSocialAuth";
-import { useNavigate } from "react-router-dom";
-
+import { useState } from 'react';
+import SignupForm from '@/shared/components/auth/SignupForm';
+import useSocialAuth from '@/shared/hooks/useSocialAuth';
+import { useNavigate } from 'react-router-dom';
+import SignupService from '@/shared/services/signup.service';
 
 const SignupPage = ({ userType }) => {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
-  const handleSignup = (formData) => {
-    console.log("Signup data:", formData);
-    // logic here
-    navigate(`/${userType}/verify-otp`, { state: { email: formData.email } });
+  const handleSignup = async (formData) => {
+    console.log('Signup data:', formData);
+    setLoading(true);
+
+    try {
+      await SignupService.sendOtp(formData.email);
+      navigate(`/${userType}/verify-otp`, { state: { email: formData.email } });
+    } catch (error) {
+      alert(
+        error.response?.data?.message || 'Failed to send OTP. Please try again.'
+      );
+    } finally {
+      setLoading(false);
+    }
   };
 
   const { handleGoogleLogin, handleFacebookLogin, handleXLogin } =
