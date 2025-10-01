67f322fb1293b0598594a8135d3c869ff0462aa7
nhittyn <hudinhdong@gmail.com>
2025-10-01 10:59:07 +0700

    feat: handle create account refactor: consolidate authentication services by replacing SignupService with AuthService and updating related components

--- frontend/src/shared/pages/CreateAccountPage.jsx
+++ frontend/src/shared/pages/CreateAccountPage.jsx
@@ -1,16 +1,40 @@
-import { useState } from "react";
-import useSocialAuth from "@/shared/hooks/useSocialAuth";
-import { useNavigate } from "react-router-dom";
-import CreateAccountForm from "@/shared/components/auth/CreateAccountForm";
+import { useState } from 'react';
+import useSocialAuth from '@/shared/hooks/useSocialAuth';
+import { useNavigate } from 'react-router-dom';
+import CreateAccountForm from '@/shared/components/auth/CreateAccountForm';
+import AuthService from '../services/auth.service';
 
 const CreateAccoutPage = ({ userType }) => {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
-  const handleCreateAccount = (formData) => {
-    console.log("Create account data:", formData);
-    // logic here
-    navigate(`/${userType}/complete-profile`);
+  const handleCreateAccount = async (formData) => {
+    setLoading(true);
+    try {
+      const verify_token = localStorage.getItem('verify_token');
+      if (!verify_token) {
+        throw new Error(
+          'Verification token not found. Please verify OTP first.'
+        );
+      }
+      const email = localStorage.getItem('email');
+      const role = userType === 'traveler' ? 'TRAVELER' : 'PROVIDER';
+      console.log('Create account data:', {
+        ...formData,
+        verify_token,
+        role,
+        email,
+      });
+      await AuthService.createUser({ ...formData, verify_token, role, email });
+      navigate(`/${userType}/complete-profile`);
+    } catch (error) {
+      alert(
+        error.response?.data?.message ||
+          'Failed to create account. Please try again.'
+      );
+    } finally {
+      setLoading(false);
+    }
   };
 
   const { handleGoogleLogin, handleFacebookLogin, handleXLogin } =
