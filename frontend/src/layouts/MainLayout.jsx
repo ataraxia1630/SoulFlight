1c62a4c5ec9c66d3ff7c53391533b9b709a0a824
QMinh <quangminhpham309@gmail.com>
2025-10-09 22:17:31 +0700

    chore: normalize line endings

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,14 +1,23 @@
 import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
 import Header from "./Header";
 
 const MainLayout = () => {
   return (
-    <Box sx={{ minHeight: "100vh", bgcolor: "background.default" }}>
+    <Box
+      sx={{
+        minHeight: "100vh",
+        bgcolor: "background.default",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <Header />
-      <Box sx={{ pt: { xs: 12, lg: 10 }, px: { xs: 2, lg: 2 } }}>
+      <Box sx={{ pt: { xs: 12, lg: 10 }, px: { xs: 2, lg: 2 }, flex: 1 }}>
         <Outlet />
       </Box>
+      <Footer />
     </Box>
   );
 };
