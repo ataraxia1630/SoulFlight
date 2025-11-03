835a08873719753c6a69c6f9ce7a2310fed6fff6
nhittyn <hudinhdong@gmail.com>
2025-11-03 19:27:52 +0700

    style: remove box shadow

--- frontend/src/features/business/pages/PartnerRegistration/Steps/ServiceInfoStep.jsx
+++ frontend/src/features/business/pages/PartnerRegistration/Steps/ServiceInfoStep.jsx
@@ -92,7 +92,6 @@ export default function ServiceInfoStep() {
       width="100%"
       bgcolor="white"
       borderRadius={3}
-      boxShadow="0 1px 3px rgba(0,0,0,0.1)"
       sx={{
         display: "flex",
         flexDirection: "column",
